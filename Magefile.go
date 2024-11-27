79db65618bb0dbfbca28a569921dcc0428927dbf
Edward Welch <edward.welch@grafana.com>
2024-11-27 22:53:34 +0000

    update my crude make step

--- Magefile.go
+++ Magefile.go
@@ -200,7 +200,7 @@ func (Build) All(ctx context.Context) {
 func (Build) DockerArm64(ctx context.Context) error {
 	// docker build --build-arg TARGET_PLATFORM=linux/arm64 --build-arg COMPILE_GOARCH=arm64 -t slimbean/grafana-kiosk:2022-01-07v2 -f build/Dockerfile .
 	log.Printf("Building docker...")
-	return sh.RunV("docker", "build", "--build-arg", "TARGET_PLATFORM=linux/arm64", "--build-arg", "COMPILE_GOARCH=arm64", "-t", "slimbean/grafana-kiosk:2023-11-01", "-f", "build/Dockerfile", ".")
+	return sh.RunV("docker", "build", "--build-arg", "TARGET_PLATFORM=linux/arm64", "--build-arg", "COMPILE_GOARCH=arm64", "-t", "slimbean/grafana-kiosk:2023-11-01v2", "-f", "build/Dockerfile", ".")
 }
 
 // Lint Run linter against codebase
